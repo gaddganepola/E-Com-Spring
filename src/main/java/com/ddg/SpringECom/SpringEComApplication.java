a4a43b2c9ab28dfd1f24c028f9bdad9a4c513261
gaddganepola <ganeypola@gmail.com>
2025-08-02 19:20:46 +0530

    project initialized

---
+++ src/main/java/com/ddg/SpringECom/SpringEComApplication.java
@@ -0,0 +1,13 @@
+package com.ddg.SpringECom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEComApplication.class, args);
+	}
+
+}
