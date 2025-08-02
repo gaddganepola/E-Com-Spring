3ae3a3e00d6aaaed0f0c3b25dd4fc23bc0a64797
gaddganepola <ganeypola@gmail.com>
2025-08-02 19:32:56 +0530

    develop endpoint for get all products

--- src/main/java/com/ddg/SpringECom/controller/ProductController.java
+++ src/main/java/com/ddg/SpringECom/controller/ProductController.java
@@ -1,9 +1,24 @@
 package com.ddg.SpringECom.controller;
 
+import com.ddg.SpringECom.model.Product;
+import com.ddg.SpringECom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+//    Get All Data
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
+    }
 }
--- src/main/java/com/ddg/SpringECom/service/ProductService.java
+++ src/main/java/com/ddg/SpringECom/service/ProductService.java
@@ -1,7 +1,18 @@
 package com.ddg.SpringECom.service;
 
+import com.ddg.SpringECom.model.Product;
+import com.ddg.SpringECom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
 }
