24d269726dfc7d539a7af484754433149ddb9b5d
gaddganepola <ganeypola@gmail.com>
2025-08-03 16:33:36 +0530

    develop endpoints for update and delete product

--- src/main/java/com/ddg/SpringECom/controller/ProductController.java
+++ src/main/java/com/ddg/SpringECom/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api")
@@ -54,4 +55,29 @@ public class ProductController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+//    Delete Product
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        try {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+//    Update Product
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile)  {
+        try {
+            service.updateProduct(id, product, imageFile);
+            return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
--- src/main/java/com/ddg/SpringECom/service/ProductService.java
+++ src/main/java/com/ddg/SpringECom/service/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ProductService {
@@ -33,5 +34,28 @@ public class ProductService {
         product.setImageData(imageFile.getBytes());
 
         return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        Product product = repo.findById(id).orElseThrow(() -> new NoSuchElementException("Product not found with id: " + id));
+        repo.deleteById(id);
+    }
+
+    public void updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        Product existingProduct = repo.findById(id).orElseThrow(() -> new NoSuchElementException("Product not found with id: " + id));
+
+        existingProduct.setName(product.getName());
+        existingProduct.setDescription(product.getDescription());
+        existingProduct.setBrand(product.getBrand());
+        existingProduct.setPrice(product.getPrice());
+        existingProduct.setCategory(product.getCategory());
+        existingProduct.setReleaseDate(product.getReleaseDate());
+        existingProduct.setProductAvailable(product.isProductAvailable());
+        existingProduct.setStockQuantity(product.getStockQuantity());
+        existingProduct.setImageName(imageFile.getOriginalFilename());
+        existingProduct.setImageType(imageFile.getContentType());
+        existingProduct.setImageData(imageFile.getBytes());
+
+        repo.save(existingProduct);
     }
 }
